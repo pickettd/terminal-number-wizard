d88215a7a082b323f3c3ebec2eacf3329dd7b6d5
David Pickett <pickettd@gmail.com>
2018-04-25 12:34:24 -1000

    Import number wizard logic into terminal app

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -3,16 +3,16 @@
 public class Hacker : MonoBehaviour {
 
     // Game configuration data
-    const string menuHint = "You may type menu at any time.";
-    string[] level1Passwords = { "books", "aisle", "shelf", "password", "font", "borrow" };
-    string[] level2Passwords = { "prisoner", "handcuffs", "holster", "uniform", "arrest" };
-    string[] level3Passwords = { "starfield", "telescope", "environment", "exploration", "astronauts" };
+    const string menuHint = "Options are Up, Down, Equal, or Restart";
 
     // Game state
-    int level;
     enum Screen { MainMenu, Password, Win };
     Screen currentScreen;
     string password;
+
+	int max = 1000;
+	int min = 1;
+	int guess = 500;
 
 	// Use this for initialization
 	void Start ()
@@ -24,20 +24,22 @@ public class Hacker : MonoBehaviour {
     {
         currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
-        Terminal.WriteLine("What would you like to hack into?");
-        Terminal.WriteLine("Press 1 for the local library");
-        Terminal.WriteLine("Press 2 for the police station");
-        Terminal.WriteLine("Press 3 for NASA!");
-        Terminal.WriteLine("Enter your selection:");
+
+		Terminal.WriteLine("Welcome to Number Wizard");
+		Terminal.WriteLine("Pick a number in your head,");
+		Terminal.WriteLine("but don't tell me!");
+		
+		Terminal.WriteLine("The highest number you can pick is " + max);
+		Terminal.WriteLine("The lowest number you can pick is " + min);
+		
+		Terminal.WriteLine("Is the number higher or lower than " + guess + "?");
+		Terminal.WriteLine("Up = higher, Down = lower,");
+		Terminal.WriteLine("Equal = equal");
     }
 
     void OnUserInput(string input)
     {
-        if (input == "menu") // we can always go direct to main menu
-        {
-            ShowMainMenu();
-        }
-        else if (input == "quit" || input == "close" || input == "exit")
+        if (input == "quit" || input == "close" || input == "exit")
         {
             Terminal.WriteLine("If on the web close the tab.");
             Application.Quit();
@@ -46,19 +48,25 @@ public class Hacker : MonoBehaviour {
         {
             RunMainMenu(input);
         }
-        else if (currentScreen == Screen.Password)
-        {
-            CheckPassword(input);
-        }
     }
 
     void RunMainMenu(string input)
     {
-        bool isValidLevelNumber = (input == "1" || input == "2" || input == "3");
-        if (isValidLevelNumber)
-        {
-            level = int.Parse(input);
-            AskForPassword();
+		if (input == "Up" || input == "up") {
+			min = guess;
+			NextGuess();
+        }
+		else if (input == "Down" || input == "down") {
+			max = guess;
+			NextGuess();
+        }
+		else if (input == "Equal" || input == "equal") {
+			Terminal.WriteLine("I won!");
+			Terminal.WriteLine("Type Restart to play again.");
+			//ShowMainMenu ();
+        }
+		else if (input == "Restart" || input == "restart") {
+			ShowMainMenu ();
         }
         else if (input == "007") // easter egg
         {
@@ -66,97 +74,15 @@ public class Hacker : MonoBehaviour {
         }
         else
         {
-            Terminal.WriteLine("Please choose a valid level");
+            Terminal.WriteLine("Please choose a valid option");
             Terminal.WriteLine(menuHint);
         }
     }
+	void NextGuess () {
+		guess = (max + min) / 2;
+		Terminal.WriteLine("Higher or lower than " + guess);
+		Terminal.WriteLine("Up = higher, Down = lower,");
+		Terminal.WriteLine("Enter = equal");
+	}
 
-    void AskForPassword()
-    {
-        currentScreen = Screen.Password;
-        Terminal.ClearScreen();
-        SetRandomPassword();
-        Terminal.WriteLine("Enter your password, hint: " + password.Anagram());
-        Terminal.WriteLine(menuHint);
-    }
-
-    void SetRandomPassword()
-    {
-        switch (level)
-        {
-            case 1:
-                password = level1Passwords[Random.Range(0, level1Passwords.Length)];
-                break;
-            case 2:
-                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
-                break;
-            case 3:
-                password = level3Passwords[Random.Range(0, level3Passwords.Length)];
-                break;
-            default:
-                Debug.LogError("Invalid level number");
-                break;
-        }
-    }
-
-    void CheckPassword(string input)
-    {
-        if (input == password)
-        {
-            DisplayWinScreen();
-        }
-        else
-        {
-            AskForPassword();
-        }
-    }
-
-    void DisplayWinScreen()
-    {
-        currentScreen = Screen.Win;
-        Terminal.ClearScreen();
-        ShowLevelReward();
-        Terminal.WriteLine(menuHint);
-    }
-
-    void ShowLevelReward()
-    {
-        switch (level)
-        {
-            case 1:
-                Terminal.WriteLine("Have a book...");
-                Terminal.WriteLine(@"
-    _______
-   /      //
-  /      //
- /_____ //
-(______(/           
-"
-                );
-                break;
-            case 2:
-                Terminal.WriteLine("You got the prison key!");
-                Terminal.WriteLine("Play again for a greater challenge.");
-                Terminal.WriteLine(@"
- __
-/0 \_______
-\__/-=' = '         
-"
-                );
-                break;
-            case 3:
-                Terminal.WriteLine(@"
- _ __   __ _ ___  __ _
-| '_ \ / _` / __|/ _` |
-| | | | (_| \__ \ (_| |
-|_| |_|\__,_|___)\__,_|
-"
-                );
-                Terminal.WriteLine("Welcome to NASA's internal system!");
-                break;
-            default:
-                Debug.LogError("Invalid level reached");
-                break;
-        }
-    }
 }
